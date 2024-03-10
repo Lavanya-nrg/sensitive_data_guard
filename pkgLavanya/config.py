719cafe9d8aca00a005e26303933861cf18ea66b
Lavanya Narang <lavanya.narang@dataverze.ai>
2024-03-10 10:49:29 +0530

    renamed folder

--- pkgLavanya/config.py
+++
@@ -1,8 +0,0 @@
-patterns = [
-    r"^Username:[a-zA-Z0-9_@.-]{3,}$",
-    r"^Password:[a-zA-Z\d@!#$%^&*()-_+=]{8,}$",
-    r"^SSN:\d{3}-\d{2}-\d{4}$",
-    r"^CreditCard:\d{4}-\d{4}-\d{4}-\d{4}$",
-    r"^MedicalRecord:MR\d+$",
-    r"^FingerprintMatching:FingerprintData\d+$",
-]
